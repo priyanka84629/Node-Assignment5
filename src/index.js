76efdda75becb11b5bda269b6cf03b5cfeb87bfa
Priyanka Gangwar <priyankagang82@gmail.com>
2023-02-23 16:40:37 +0530

    node-Assignment5 is pushed

---
+++ src/index.js
@@ -0,0 +1,25 @@
+var http = require("http");
+const httpServer = http.createServer(handleServer);
+
+function handleServer(req, res) {
+    if (req.url == '/welcome') {
+        res.writeHead(200, { 'content-type': 'text/plain' });
+        res.write('Welcome to Dominos!');
+        res.end();
+    }
+    else if (req.url == '/contact') {
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.write(JSON.stringify({
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+        }));
+        res.end();
+    }
+    else {
+        res.writeHead(404, { 'content-type': 'text/plain' });
+        res.write('Invalid Request.........');
+        res.end();
+    }
+}
+module.exports = httpServer;
+httpServer.listen(8081);
